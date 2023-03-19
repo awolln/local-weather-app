13f9047a04eea54be68684bc597863d86069c43c
awolln <aweln457@gmail.com>
2023-03-19 00:07:50 -0700

    change version and html component

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent {
+  
+
+}
